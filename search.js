f54fad40f97fe519d757fbdcd91cc32c943cfd65
josefervalenciacontreras <fernando.valencia@est.iudigital.edu.co>
2023-06-12 19:18:58 -0500

    agregando leyendas de validación al formulario

--- search.js
+++ search.js
@@ -1,16 +1,65 @@
+//En este codigo se estaba probando la funcionalidad del botón buscar
+/*function myFunction(){
+    var texto = "Hola Mundo";
+    document.write(texto);
+
+}*/
+
+
+function validar(){
+    var nombre = document.getElementById("formGroupExampleInput");
+    var descripcion = document.getElementById("formGroupExampleInput2");
+    var imagen = document.getElementById("formGroupExampleInput3");
+    var nombreRequerido = document.getElementById("nombre-requerido");
+    var descRequerido = document.getElementById("descripcion-requerido");
+    var imgRequerido = document.getElementById("imagen-requerido");
+
+    if(nombre.value == ""){
+        nombreRequerido.innerText = "Campo requerido";
+    }else{
+        nombreRequerido.innerText = "";
+    }
+    if(descripcion.value == ""){
+        descRequerido.innerText = "Campo requerido";
+    }else{
+        descRequerido.innerText = "";
+    }
+    if(imagen.value == ""){
+        imgRequerido.innerText = "Campo requerido";
+    }else{
+        imgRequerido.innerText = "";
+    }
+
+    if (nombre.value !== "" && descripcion.value !== "" && imagen.value !== "") {
+        alert("¡Restaurante creado con éxito!");
+    }
+}
+
+//Codigo para hacer funcionar el botón buscar pero no se implementa porque no me funciona
+/*
+let cards = document.querySelectorAll('.descripcion')
+    
 function myFunction() {
-    var input, filter, div, li, a, i, txtValue,col;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementsByClassName("row");
-    col = div.getElementByClassName("col");
-    for (i = 0; i < col.length; i++) {
-        a = col[i].getElementsByClassName("row")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            col[i].style.display = "";
+    let search_query = document.getElementById("myInput").value;
+    
+    //Use innerText if all contents are visible
+    //Use textContent for including hidden elements
+    for (var i = 0; i < cards.length; i++) {
+        if(cards[i].textContent.toLowerCase()
+                .includes(search_query.toLowerCase())) {
+            cards[i].classList.remove("is-hidden");
         } else {
-            col[i].style.display = "none";
+            cards[i].classList.add("is-hidden");
         }
     }
 }
+
+//A little delay
+let typingTimer;               
+let typeInterval = 500;  
+let searchInput = document.getElementById('myInput');
+
+searchInput.addEventListener('keyup', () => {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(myFunction, typeInterval);
+});*/
